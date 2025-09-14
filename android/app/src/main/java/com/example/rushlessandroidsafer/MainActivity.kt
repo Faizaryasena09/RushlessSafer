1c450c94f55d303df0adfdad8f97f33d115f9c05
Faizaryasena09 <cenax09@gmail.com>
2025-09-14 10:22:24 +0700

    haihai

---
+++ android/app/src/main/java/com/example/rushlessandroidsafer/MainActivity.kt
@@ -0,0 +1,24 @@
+package com.example.rushlessandroidsafer
+
+import android.os.Bundle
+import android.webkit.WebView
+import android.webkit.WebViewClient
+import androidx.appcompat.app.AppCompatActivity
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val webView: WebView = findViewById(R.id.webview)
+        webView.webViewClient = WebViewClient()
+        webView.settings.javaScriptEnabled = true
+        webView.loadUrl("https://www.google.com")
+    }
+
+    override fun onStart() {
+        super.onStart()
+        startLockTask()
+    }
+}
--- obj/Debug/net8.0-windows/RushlessSafer.AssemblyInfo.cs
+++ obj/Debug/net8.0-windows/RushlessSafer.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Rushless Safer")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+586c58d19b3afe86a7a7abc90bdc5b007856378e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7df65a11c03f97a89f6181d91c49783cf62f31e1")]
 [assembly: System.Reflection.AssemblyProductAttribute("Rushless Safer")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Rushless Safer")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
